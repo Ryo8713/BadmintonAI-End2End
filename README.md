1521656f584c4495907f8ce25f6c1094042b4d0f
omei_ogami <michaelmick05220806@gmail.com>
2025-05-09 04:24:18 +0800

    Pipe: TrackNet

--- README.md
+++ README.md
@@ -8,4 +8,6 @@
     - A folder `videos` containing rally videos will be created in the root directory of the project.
 - [x] Court detection
     - Results of `court.txt` and `court_image.png` will be stored in `court_detection/`.
-- [ ] TrackNet
+- [x] TrackNet
+    - The `traj/` folder.
+- [ ] Pose
--- main.py
+++ main.py
@@ -1,25 +1,44 @@
 from rally_clipping.get_result import timepoints_clipping
+from TrackNetV3.predict import predict_traj
+from TrackNetV3.denoise import smooth
 from pathlib import Path
 import subprocess
-
-COURT_DETECTION_PATH = 'court-detection.exe'
-COURT_OUTPUT = 'court_detection/court.txt'
-COURT_IMAGE = 'court_detection/court_image.png'
+import pandas as pd
+import os
 
 if __name__ == '__main__':
 
     video_path = Path('sample.mp4')
+    name = video_path.stem
+
+    RALLY_OUTPUT = f'videos/{name}/'
+    COURT_DETECTION_PATH = 'court_detection/court-detection.exe'
+    COURT_OUTPUT = 'court_detection/court.txt'
+    COURT_IMAGE = 'court_detection/court_image.png'
+    TRAJ_OUTPUT = 'traj/'
 
     # rally clipping
     print("\n[Message] Start rally clipping\n")
-    timepoints_clipping(video_path)
+    # timepoints_clipping(video_path)
     print("\n[Message] Rally clipping finished\n")
 
     # court detection
     print("\n[Message] Start court detection\n")
+    '''
     result = subprocess.run(
         [COURT_DETECTION_PATH, video_path, COURT_OUTPUT, COURT_IMAGE, "10"],
         capture_output=True, text=True
     )
+    '''
     print("The court information is stored in court_detection/")
     print("[Message] Court detection finished\n")
+
+    # TrackNet
+    print("\n[Message] Start trajectory prediction\n")
+    for clip in os.listdir(RALLY_OUTPUT):
+        print(clip)
+        clip_path = Path(f'{RALLY_OUTPUT}{clip}')
+        traj_csv_file = predict_traj(clip_path, TRAJ_OUTPUT)
+        df = pd.read_csv('traj/clip_1_ball.csv', encoding="utf-8")
+        smooth(traj_csv_file, df)
+    print("\n[Message] Trajectory prediction finished\n")
